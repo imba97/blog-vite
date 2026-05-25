25e6c27e0cb114e6cc24511ab0bef0c8f2bcb1c4
imba97 <mail@imba97.cn>
2026-05-25 09:39:17 +0800

    feat: add friend descriptions and new link entry

--- src/components/Firends.vue
+++ src/components/Firends.vue
@@ -21,6 +21,12 @@
           <div class="text-lg list-title list-title-hover font-semibold transition-colors duration-200">
             {{ item.name }}
           </div>
+          <div
+            v-if="item.description"
+            class="mt-1 text-sm text-muted/60 dark:text-muted/50"
+          >
+            {{ item.description }}
+          </div>
         </div>
       </div>
     </a>
@@ -37,12 +43,14 @@ const list = [
   {
     name: '言成言成啊',
     url: 'https://meethigher.top/blog',
-    avatar: 'https://meethigher.top/blog/images/favicon.ico'
+    avatar: 'https://meethigher.top/blog/images/favicon.ico',
+    description: '相遇在更高处'
   },
   {
     name: '小码博客',
     url: 'https://blog.hikki.site',
-    avatar: 'https://blog.hikki.site/data/img/avatar.jpg'
+    avatar: 'https://blog.hikki.site/data/img/avatar.jpg',
+    description: '喜欢的东西就努力去追求，万一成功了呢！'
   },
   {
     name: 'whocarewuのblog',
@@ -52,17 +60,26 @@ const list = [
   {
     name: 'PeterJXL',
     url: 'https://www.peterjxl.com',
-    avatar: 'https://image.peterjxl.com/blog/re0.jpg'
+    avatar: 'https://image.peterjxl.com/blog/re0.jpg',
+    description: '从 01 开始'
   },
   {
     name: 'imxizhen',
     url: 'https://www.imxizhen.cn',
-    avatar: 'https://q1.qlogo.cn/g?b=qq&nk=89525295&s=640'
+    avatar: 'https://q1.qlogo.cn/g?b=qq&nk=89525295&s=640',
+    description: '永远相信美好的事情即将发生'
   },
   {
     name: 'ZMTU',
     url: 'https://zmtu.com',
-    avatar: 'https://zmtu.com/static/images/app/App_Logo.png'
+    avatar: 'https://zmtu.com/static/images/app/App_Logo.png',
+    description: 'ACG二次元社区，我们的征途是星辰大海！'
+  },
+  {
+    name: 'UpXuu’s blog',
+    url: 'https://upxuu.com',
+    avatar: 'https://upxuu.com/images/20260214145619.jpg',
+    description: '逐光而上'
   }
 ]
 </script>
