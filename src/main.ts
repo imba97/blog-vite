6a22e1b0d1db5febbe85781f7cd82552ad2620ec
imba97 <mail@imba97.cn>
2026-06-24 00:08:47 +0800

    chore: update dependencies

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ import 'markdown-it-github-alerts/styles/github-colors-light.css'
 import 'markdown-it-github-alerts/styles/github-colors-dark-class.css'
 import 'markdown-it-github-alerts/styles/github-base.css'
 import '@shikijs/twoslash/style-rich.css'
-import 'shiki-magic-move/style.css'
+import '@shikijs/magic-move/style.css'
 import 'uno.css'
 
 export const createApp = ViteSSG(
